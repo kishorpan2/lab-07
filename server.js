7024ae9c4f163341875d38a07acaf0fd4033434b
Kishor Pandey <kishor.jpandey@gmail.com>
2019-05-04 20:03:15 -0700

    redeploy

--- server.js
+++ server.js
@@ -31,44 +31,33 @@ app.listen(port,() => console.log(`Listening on port ${port}`));
  * Handlers
  */
 function getLocation(request, response) {
-    const query = request.query.data;
-    let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${ query }&key=${ process.env.GEOCODE_API_KEY }`;
+  const query = request.query.data;
+  let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${ query }&key=${ process.env.GEOCODE_API_KEY }`;
 
-    return superagent.get(geocodeURL)
-      .then((apiResponse) => response.send(new Location(query, apiResponse.body.results[0])))
-      .catch((error) => handleError(error, request, response));
+  return superagent.get(geocodeURL)
+    .then((apiResponse) => response.send(new Location(query, apiResponse.body.results[0])))
+    .catch((error) => handleError(error, request, response));
 }
 
 function getWeather(request, response) {
-<<<<<<< HEAD
-  try {
-    //let weatherData = require('./data/darksky.json');
-    // let weatherData = `https://maps.googleapis.com/maps/api/geocode/json?query=YOUR_API_KEY`;
-    // let weatherObjects = weatherData.daily.data.map((day) => new Weather(day));
-    // response.send(weatherObjects);
-  }
-  catch(error) { 
-    console.log(error);
-    response.status(500).send('Status 500: I done messed up.');
-=======
-    let latitude = request.query.data.latitude;
-    let longitude = request.query.data.longitude;
-    let weatherURL = `https://api.darksky.net/forecast/${ process.env.WEATHER_API_KEY }/${ latitude },${ longitude }`;
+  let latitude = request.query.data.latitude;
+  let longitude = request.query.data.longitude;
+  let weatherURL = `https://api.darksky.net/forecast/${ process.env.WEATHER_API_KEY }/${ latitude },${ longitude }`;
 
-    return superagent.get(weatherURL)
-      .then((apiResponse) => response.send(apiResponse.body.daily.data.map((day) => new Weather(day))))
-      .catch((error) => handleError(error, request, response));
+  return superagent.get(weatherURL)
+    .then((apiResponse) => response.send(apiResponse.body.daily.data.map((day) => new Weather(day))))
+    .catch((error) => handleError(error, request, response));
 }
 
 function getEvents (request, response) {
-    let latitude = request.query.data.latitude;
-    let longitude = request.query.data.longitude;
-    let eventURL = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${ longitude }&location.latitude=${ latitude }&location.within=25km&expand=venue`;
+  let latitude = request.query.data.latitude;
+  let longitude = request.query.data.longitude;
+  let eventURL = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${ longitude }&location.latitude=${ latitude }&location.within=25km&expand=venue`;
 
-    return superagent.get(eventURL)
-      .set('Authorization', `Bearer ${ process.env.EVENTBRITE_API_KEY }`)
-      .then((apiResponse) => response.send(apiResponse.body.events.map((event) => new Event(event))))
-      .catch((error) => handleError(error, request, response));
+  return superagent.get(eventURL)
+    .set('Authorization', `Bearer ${ process.env.EVENTBRITE_API_KEY }`)
+    .then((apiResponse) => response.send(apiResponse.body.events.map((event) => new Event(event))))
+    .catch((error) => handleError(error, request, response));
 }
 
 function handleError(err, req, res) {
@@ -76,7 +65,6 @@ function handleError(err, req, res) {
 
   if (res) {
     res.status(500).send('Status 500: I done messed up.');
->>>>>>> ff959328fe5c7588a4845b4506d5a644822f0dcf
   }
 }
 
